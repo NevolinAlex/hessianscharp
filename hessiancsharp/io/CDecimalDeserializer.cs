f151848ac57fc17c1c68eed244d591f0f7f637b9
nevolinalex <lapusha.nevolin@gmail.com>
2018-02-06 16:47:34 +0500

    init commit

---
+++ hessiancsharp/io/CDecimalDeserializer.cs
@@ -0,0 +1,41 @@
+using System;
+using System.IO;
+
+namespace hessiancsharp.io
+{
+	/// <summary>
+	/// Summary description for CDecimalDeserializer.
+	/// </summary>
+	public class CDecimalDeserializer : AbstractDeserializer
+	{
+		public override object ReadMap(AbstractHessianInput abstractHessianInput)
+		{
+			String strInitValue = null;
+			
+			while (!abstractHessianInput.IsEnd())
+			{
+				string strKey = abstractHessianInput.ReadString();
+				string strValue = abstractHessianInput.ReadString();
+				
+				if (strKey.Equals("value") || strKey.Equals("stringCache"))
+					strInitValue = strValue;
+			}
+			
+			abstractHessianInput.ReadMapEnd();
+			
+			if (strInitValue == null)
+				throw new IOException("No value found for decimal.");
+			
+			return Decimal.Parse(strInitValue);
+		}
+		public override object ReadObject(AbstractHessianInput abstractHessianInput)
+		{
+			String strInitValue = abstractHessianInput.ReadString();
+
+			//if (strInitValue == null)
+			//	throw new IOException("No value found for decimal.");
+
+			return Decimal.Parse(strInitValue);
+		}
+	}
+}
