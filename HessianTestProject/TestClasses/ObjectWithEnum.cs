3068bc02fb3e7ca5c35268e8f9f73765bc382156
nevolinalex <lapusha.nevolin@gmail.com>
2018-02-07 13:48:01 +0500

    add test project

---
+++ HessianTestProject/TestClasses/ObjectWithEnum.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HessianTestProject.TestClasses
+{
+	class ObjectWithEnum
+	{
+		private TestEnum testEnum;
+
+		public ObjectWithEnum()
+		{
+			testEnum = TestEnum.testing;
+		}
+
+		public override bool Equals(object obj)
+		{
+			var @enum = obj as ObjectWithEnum;
+			return @enum != null &&
+				   testEnum == @enum.testEnum;
+		}
+
+		public override int GetHashCode()
+		{
+			return 337929216 + testEnum.GetHashCode();
+		}
+	}
+}
---
+++ HessianTestProject/TrashTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using hessiancsharp.io;
+using HessianTestProject.TestClasses;
+using NUnit.Framework;
+
+namespace HessianTestProject
+{
+	public class TrashTests
+	{
+		[Test]
+		public void TestEnumSerializing()
+		{
+			MemoryStream stream = new MemoryStream();
+			CHessianOutput out1 = new CHessianOutput(stream);
+
+			ObjectWithEnum obj = new ObjectWithEnum();
+
+			out1.StartReply();
+			out1.WriteObject(obj);
+			out1.CompleteReply();
+			byte[] data = stream.ToArray();
+
+			File.WriteAllBytes(@"C:\Users\disap\Desktop\Projects\hessianscharp\HessianTestProject\test.txt", data);
+			CHessianInput input = new CHessianInput(new MemoryStream(data));
+			var result = input.ReadReply(typeof(ObjectWithEnum));
+			Assert.IsTrue(true);
+		}
+	}
+}
