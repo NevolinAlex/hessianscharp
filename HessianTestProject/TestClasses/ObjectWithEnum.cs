208114109c284b4f9d73d912fd8a09d552d7c274
nevolinalex <lapusha.nevolin@gmail.com>
2018-02-07 13:34:01 +0500

    change location

---
+++ HessianTestProject/TestClasses/ObjectWithEnum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HessianTestProject.TestClasses
+{
+	class ObjectWithEnum
+	{
+		public TestEnum testEnum;
+
+		public ObjectWithEnum()
+		{
+			testEnum = TestEnum.testing;
+		}
+	}
+}
